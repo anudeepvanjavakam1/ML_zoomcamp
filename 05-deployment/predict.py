61a055dbe3f0cd727a76d27f6b318c950c6ce887
anudeepvanjavakam1 <anudeepvanjavakam@gmail.com>
2023-10-15 06:49:17 +0000

    added docker, pipenv and sample prediction files

---
+++ 05-deployment/predict.py
@@ -0,0 +1,27 @@
+import pickle
+from flask import Flask
+from flask import request
+
+with open("model2.bin", 'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open("dv.bin", 'rb') as f_in:
+    dv = pickle.load(f_in)
+
+app = Flask('get-credit-prob')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    customer = request.get_json()
+
+    X = dv.transform([customer])
+
+    y_pred = model.predict_proba(X)[0,1]
+    
+    result = {'probability that the client gets credit': float(y_pred)}
+
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
